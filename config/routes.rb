834af976354b3b05220d4d1b8046fc8200279e00
Taylor Varoglu <taylor.varoglu@gmail.com>
2025-07-26 17:18:19 -0700

    Comment out auto-generated route scaffold in routes.rb and add TODO comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # TODO: update routes on subsequent feature branch
+  # resources :toppings
+  # resources :pizzas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
