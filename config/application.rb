4389a9a0ce7fdfd176e6156bc0792dbd3a592ad7
Taylor Varoglu <taylor.varoglu@gmail.com>
2025-07-27 13:09:49 -0700

    Update config/application.rb following rails version downgrade

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
+require "logger" # ensures Logger is available for Ruby 3.2+ with Rails 7.0.x
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,12 +22,12 @@ Bundler.require(*Rails.groups)
 module PeetZahMgr
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 7.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    # config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
