52c6eb93726fa727c78029206bf2b518e550d1e7
Taylor Varoglu <taylor.varoglu@gmail.com>
2025-07-27 16:27:06 -0700

    Update model specs to enforce additional validations

--- spec/models/pizza_spec.rb
+++ spec/models/pizza_spec.rb
@@ -1,10 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Pizza, type: :model do
-  subject { Pizza.create!(name: "Test Pizza") }
+  let!(:cheese) { Topping.create!(name: "Cheese") }
+
+  subject do
+    Pizza.create!(name: "Test Pizza", toppings: [ cheese ])
+  end
 
   it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name).case_insensitive }
+
+  it "validates case-insensitive uniqueness of name" do
+    Pizza.create!(name: "Unique Pizza", toppings: [ cheese ])
+    dup = Pizza.new(name: "unique pizza", toppings: [ cheese ])
+    expect(dup.valid?).to be false
+    expect(dup.errors[:name]).to include("has already been taken")
+  end
+
   it { should have_many(:pizza_toppings).dependent(:destroy) }
   it { should have_many(:toppings).through(:pizza_toppings) }
+
+  it "capitalizes name before saving" do
+    pizza = Pizza.create!(name: "test pizza", toppings: [ cheese ])
+    expect(pizza.name).to eq("Test Pizza")
+  end
+
+  it "requires at least one topping" do
+    pizza = Pizza.new(name: "No Toppings")
+    expect(pizza.valid?).to be false
+    expect(pizza.errors[:toppings]).to include("must include at least one topping")
+  end
+
+  it "prevents duplicate topping sets regardless of name" do
+    Pizza.create!(name: "Combo Pizza", toppings: [ cheese ])
+    dup = Pizza.new(name: "Another Name", toppings: [ cheese ])
+    expect(dup.valid?).to be false
+    expect(dup.errors[:base]).to include("A pizza with the same set of toppings already exists")
+  end
 end
--- spec/models/topping_spec.rb
+++ spec/models/topping_spec.rb
@@ -5,6 +5,17 @@ RSpec.describe Topping, type: :model do
 
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name).case_insensitive }
-  it { should have_many(:pizza_toppings).dependent(:destroy) }
+  it { should have_many(:pizza_toppings).dependent(:restrict_with_error) }
   it { should have_many(:pizzas).through(:pizza_toppings) }
+
+  it "normalizes name capitalization" do
+    t = Topping.create!(name: "  pepperoni  ")
+    expect(t.name).to eq("Pepperoni")
+  end
+
+  it "prevents deletion if used on a pizza" do
+    pizza = Pizza.create!(name: "Test Pizza", toppings: [ subject ])
+    expect(subject.destroy).to be false
+    expect(subject.errors[:base]).to include("Cannot delete record because dependent pizza toppings exist")
+  end
 end
