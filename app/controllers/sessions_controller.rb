280d640ae77c081d379285554abed85110d39230
Taylor Varoglu <taylor.varoglu@gmail.com>
2025-07-28 02:35:55 -0700

    Fix redirect edge case in sessions_controller.rb for unauthorized users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    redirect_to pizzas_path and return if current_user
+  end
 
   def create
     user = User.find_by(username: params[:username])
